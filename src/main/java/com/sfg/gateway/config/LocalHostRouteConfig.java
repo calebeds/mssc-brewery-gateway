46287eb7e4f584bf87a4f0f68fe12712d07c1f34
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-10-10 17:37:15 -0300

    add route for orders - close #2

--- src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
+++ src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
@@ -12,7 +12,10 @@ public class LocalHostRouteConfig {
         return builder.routes()
                 .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
                         .uri("http://localhost:8080")
-                        .id("beerservice"))
+                        .id("beer-service"))
+                .route(r -> r.path("/api/v1/customers/**")
+                        .uri("http://localhost:8081")
+                        .id("order-service"))
                 .build();
     }
 }
