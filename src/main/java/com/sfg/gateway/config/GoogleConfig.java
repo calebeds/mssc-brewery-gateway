de79e9b06bbd3f2668cfb82dbf880386a525e9eb
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-10-10 17:37:08 -0300

    add beer service route config

--- src/main/java/com/sfg/gateway/config/GoogleConfig.java
+++ src/main/java/com/sfg/gateway/config/GoogleConfig.java
@@ -6,9 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("google")
-@Configuration
+//@Profile("google")
+//@Configuration
 public class GoogleConfig {
+    /**
+     * This is just an example of routing requests to Google from localhost. For demonstration only
+     * @param builder
+     * @return
+     */
     @Bean
     public RouteLocator googleRouteConfig(RouteLocatorBuilder builder) {
         return builder.routes()
